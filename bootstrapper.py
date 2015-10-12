f8e2dc72fd14ece0be824e8628e562227c28b670
throsturx <sparog+github@gmail.com>
2015-10-12 23:17:15 +0000

    allow comparison in bootstrapper

--- bootstrapper.py
+++ bootstrapper.py
@@ -6,8 +6,15 @@ import agent
 import evaluator
 import summarizer
 from game import Game
+from plotter import evaluation_plot, compare_evals
 
-def main():
+def comparison():
+    e1 = evaluate(args.solution_name)
+    exercise = __import__(args.compare_to)
+    e2 = evaluate(args.compare_to)
+    compare_evals(e1, e2)
+
+def evaluate(name):
     # game and agent setup code
     game = Game()
     game.set_size(args.grid_size, args.grid_size)
@@ -20,20 +27,35 @@ def main():
     # clean up after training
     agent.accumulated = 0  # reset accumulated rewards
     agent.set_epsilon(0.0) # turn off exploration
+    agent.game.reset()
     # evaluate the training results
-    file_name = evaluator.evaluate(agent)
+    file_name = evaluator.evaluate(agent, name='eval_'+name) 
     # print out a nice summary of how the evaluation went
     summarizer.summarize_e(file_name)
+    return file_name
+
+def main():
+    file_name = evaluate(args.solution_name)
+    evaluation_plot(file_name)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='RL Learner exercise')
     parser.add_argument('solution_name', default='exercise', nargs='?', help='exercise to evaluate')
+    parser.add_argument('-c', '--compare_to', help='compare two solutions')
     parser.add_argument('-g', '--grid_size', type=int, default=7, help='grid size')
+    parser.add_argument('--debug', action='store_true')
     args = parser.parse_args()
     # import the solution name into the global namespace as 'exercise'
     exercise = __import__(args.solution_name)
     try:
-        main()
+        if args.debug:
+            summarizer.summarize_e('evaluation.txt')
+            compare_evals('eval_exercise.txt', 'eval_ex2.txt')
+            exit(0)
+        if args.compare_to:
+            comparison()
+        else:
+            main()
     except SystemExit:
         exit(1)
     except KeyboardInterrupt:
