b700a9118e7275f1ee15ecc9ba7efe8f8bf781be
throsturx <sparog+github@gmail.com>
2015-10-15 16:27:30 +0000

    exercise improved

--- exercise.py
+++ exercise.py
@@ -1,19 +1,23 @@
 import learners
 import agent
 
-# train the agent before evaluation
-def train(agent):
-    """This method should be implemented by the student
-    An example implementation is provided."""
-    print 'Training {0}...'.format(agent)
-    agent.set_epsilon(0.1)  # set the exploration
-    for i in xrange(3000):  # train it for 1000 steps
-        agent.perform()     # do an action
-
 # select the agent here
 def get_agent(game):
+    """Modify this method to create the appropriate agent"""
     actions = ['left', 'forward', 'right']
+    # Use a MetaAgent with QLearners at leaf nodes
     player = agent.MetaAgent(game, actions, epsilon=0.1, fov=3, learner_class=learners.QLearn)
+    # Use a flat Agent with the default SARSA learner
 #   player = agent.Agent(game, actions, epsilon=0.1, fov=3)
     return player
+
+
+# train the agent before evaluation
+def train(player):
+    """This method should be implemented by the student
+    An example implementation is provided."""
+    print 'Training {0}...'.format(player)
+    player.set_epsilon(0.1)  # set the exploration
+    for i in xrange(3000):   # train it for 3000 steps
+        player.perform()     # do an action at every step
 
