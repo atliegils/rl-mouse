5eec15733f77b54843602d26865df476fc27110d
throsturx <sparog+github@gmail.com>
2015-10-16 12:38:55 +0000

    minor changes

--- summarizer.py
+++ summarizer.py
@@ -36,12 +36,12 @@ def summarize_e(name):
     print 'High Score: {0}'.format(high_score)
     print 'Total extra steps: {0}'.format(extra_steps)
     print 'Average performance: {0}'.format(average_ratio)
-    magic = (accumulated_reward / (extra_steps)) * average_ratio 
+    magic = ((accumulated_reward - extra_steps * 0.2) / (extra_steps)) * average_ratio 
 #   magic = ((best_local * 2 - deaths * 3) * (high_score + accumulated_reward / high_score) * 0.001 + (high_score * 0.001) - extra_steps * 0.001 - timeouts) * average_ratio
     print 'Evaluation score: {0}'.format(magic)
     print '\t',
 #   if magic < 100 or timeouts > 100 or accumulated_reward < 0 or average_ratio < 0.3:
-    if magic <= 0:
+    if magic <= 0.01:
         print 'FAIL'
     else:
         print 'PASS'
