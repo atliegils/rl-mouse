ad36224a8c578a657da9995d2ad06c04f4ef842a
throsturx <sparog+github@gmail.com>
2015-10-20 15:25:59 +0000

    `solution` directory support + gamma adjustment

--- bootstrapper.py
+++ bootstrapper.py
@@ -3,7 +3,7 @@ import argparse
 import copy
 import evaluator
 import summarizer
-import traceback
+import sys, traceback
 from game import Game
 from plotter import evaluation_plot, compare_evals
 from  functools import partial
@@ -14,7 +14,7 @@ def comparison():
     compare_evals(e1, e2)
 
 def convert(name):
-    return name.rstrip('.py').replace('/','.')
+    return name.rstrip('.py').replace('/','.').replace('solutions.','')
 
 def evaluate(name, no_initial_training=False):
     # import the solution name into the global namespace as 'exercise'
@@ -27,6 +27,7 @@ def evaluate(name, no_initial_training=False):
     agent = exercise.get_agent(game)
     agent.reward_scaling([1, -1, -1])
     agent.fov = args.fov
+    agent.gamma = args.gamma
     agent.game.suppressed = True
     # train the agent using the provided solution
     exercise.reward_profile(agent)
@@ -43,7 +44,7 @@ def evaluate(name, no_initial_training=False):
     agent.set_epsilon(0.0)  # turn off exploration
     agent.game.reset()      # reset the game
     agent.game.high_score = 0
-    agent.fov = args.fov
+    agent.fov  = args.fov
     agent.game = original_game # if the training modifies the game, it is fixed here
     exercise.reward_profile(agent)
     if args.dephase:
@@ -62,24 +63,27 @@ def main():
         file_name2 = evaluate(args.solution_name, no_initial_training=True)
         compare_evals(file_name, file_name2)
     else:
+        if args.outfile and os.path.exists(args.outfile):
+            raise OSError('The destination file already exists, move it or pick another name.')
         file_name = evaluate(args.solution_name)
+        if args.outfile:
+            os.rename(file_name, args.outfile)
+            file_name = args.outfile
+
         evaluation_plot(file_name)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='RL Learner exercise')
     parser.add_argument('solution_name', default='exercise', nargs='?', help='exercise to evaluate')
-    parser.add_argument('-c', '--compare_to', help='compare two solutions')
+    parser.add_argument('--outfile', nargs='?', help='output file name')
     parser.add_argument('-g', '--grid_size', type=int, default=10, help='grid size')
     parser.add_argument('-f', '--fov', type=int, default=3, help='base field of view')
+    parser.add_argument('--gamma', type=float, default=0.8, metavar='FLOAT', help='discount factor')
+    parser.add_argument('-c', '--compare_to', metavar='OTHER_FILE', help='compare two solutions')
     parser.add_argument('--dephase', action='store_true', help=u'swap reward scalars (180\N{DEGREE SIGN} out of phase)')
-    parser.add_argument('--debug', action='store_true')
     args = parser.parse_args()
+    sys.path.insert(0, 'solutions')
     try:
-        if args.debug:
-#           summarizer.summarize_e('evaluation.txt')
-#           compare_evals('eval_exercise.txt', 'eval_ex2.txt')
-            print 'args.debug'
-            exit(0)
         if args.compare_to:
             comparison()
         else:
