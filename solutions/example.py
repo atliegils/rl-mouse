7418b64af9eb53703fa6d83650d60f810261242b
Jordi Bieger <jbieger@gmail.com>
2016-01-14 15:52:42 +0000

    Changed epsilon to exploration_rate in example.py

--- solutions/example.py
+++ solutions/example.py
@@ -6,7 +6,7 @@ def get_agent(game):
     """Modify this method to create the appropriate agent"""
     actions = ['left', 'forward', 'right']
     # Use a MetaAgent with QLearners at leaf nodes
-    player = agent.MetaAgent(game, actions, epsilon=0.1, fov=3, learner_class=learners.QLearn)
+    player = agent.MetaAgent(game, actions, exploration_rate=0.1, fov=3, learner_class=learners.QLearn)
     # Use a flat Agent with the default SARSA learner
 #   player = agent.Agent(game, actions, epsilon=0.1, fov=3)
     return player
@@ -17,7 +17,7 @@ def train(player):
     """This method should be implemented by the student
     An example implementation is provided."""
     print 'Training {0}...'.format(player)
-    player.set_epsilon(0.1)  # set the exploration
+    player.set_exploration_rate(0.1)  # set the exploration
     for i in xrange(3000):   # train it for 3000 steps
         player.perform()     # do an action at every step
 
