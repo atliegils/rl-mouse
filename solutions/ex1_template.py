bd9cf7ae0cdfc79b677c6bc7c2245cf2b8cd1a43
Jordi Bieger <jbieger@gmail.com>
2016-01-15 00:42:39 +0000

    Changes to ex1_template.py: removed reward_profile, asserted the grid is square and renamed get_policy to define_policy.

--- solutions/ex1_template.py
+++ solutions/ex1_template.py
@@ -4,6 +4,7 @@ import agent
 # select the agent here
 def get_agent(game):
     """Modify this method to create the appropriate agent"""
+    assert game.width == game.height, 'not a square grid'
     actions = ['left', 'forward', 'right']
     player = agent.DeterministicAgent(game, actions)
     w = game.width
@@ -12,12 +13,12 @@ def get_agent(game):
         for ty in range(h):
             for cx in range(w):
                 for cy in range(h):
-                    player.policy[(cx,cy,tx,ty)] = get_policy(cx,cy,tx,ty,w,h)
+                    player.policy[(cx,cy,tx,ty)] = define_policy(cx,cy,tx,ty,w,h)
 
     return player
 
-def get_policy(cx,cy,tx,ty,w,h):
-    """Get the policy for the situation where the cheese is at [cx,cy],
+def define_policy(cx,cy,tx,ty,w,h):
+    """Defines the policy for the situation where the cheese is at [cx,cy],
     the trap is at [tx,ty], and the grid has width w and height h (all
     relative to the mouse)."""
 
@@ -29,12 +30,3 @@ def get_policy(cx,cy,tx,ty,w,h):
 # train the agent before evaluation, but not in this exercise
 def train(player):
     return
-
-# control over rewards for (finding cheese, entering a trap, walking)
-# by default the rewards are scaled by [1, -1, -1]
-def reward_profile(player):
-    """The reward profile can be adjusted by changing the line below
-    Hint: Flat learners sometimes require a little extra push --
-    by punishing the learner every step it learns not to walk in circles"""
-    player.adjust_rewards(2,3,1)
-
