678c217e884cbc973cc835e036c52d534795645c
Jordi Bieger <jbieger@gmail.com>
2016-01-22 04:03:58 +0000

    Added template for Exercise 2 and slightly changed example.py.

---
+++ solutions/ex2_template.py
@@ -0,0 +1,21 @@
+import learners
+import agent
+
+# select the agent here
+def get_agent(game):
+    """Modify this method to create the appropriate agent"""
+    actions = ['left', 'forward', 'right']
+    player = agent.OmniscientMouseAgent(game, actions, learner_class=learners.QLearn)
+    return player
+
+
+# train the agent before evaluation
+def train(player):
+    """This method should be implemented by the student
+    An example implementation is provided."""
+    print 'Training...'
+    player.set_exploration_rate(0.25)  # set the exploration
+    for i in xrange(1000):  # train it for 1000 steps
+        player.perform()     # do an action at every step
+    player.set_exploration_rate(0.0) # turn exploration off for evaluation
+
--- solutions/example.py
+++ solutions/example.py
@@ -6,9 +6,9 @@ def get_agent(game):
     """Modify this method to create the appropriate agent"""
     actions = ['left', 'forward', 'right']
     # Use a MetaMouseAgent with QLearners at leaf nodes
-    player = agent.MetaMouseAgent(game, actions, exploration_rate=0.1, fov=3, learner_class=learners.QLearn)
+    # player = agent.MetaMouseAgent(game, actions, exploration_rate=0.1, fov=3, learner_class=learners.QLearn)
     # Use a flat MouseAgent with the default SARSA learner
-#   player = agent.MouseAgent(game, actions, epsilon=0.1, fov=3)
+    player = agent.MouseAgent(game, actions, epsilon=0.1, fov=3)
     return player
 
 
@@ -16,8 +16,9 @@ def get_agent(game):
 def train(player):
     """This method should be implemented by the student
     An example implementation is provided."""
-    print 'Training {0}...'.format(player)
+    print 'Training...'
     player.set_exploration_rate(0.1)  # set the exploration
     for i in xrange(3000):   # train it for 3000 steps
         player.perform()     # do an action at every step
+    player.set_exploration_rate(0.0) # turn off exploration if you're using on-policy learning
 
