6aa86e44a90593c75f310ae28d4124265ef0c7a0
Atli <Atli@mobiz.is>
2018-10-04 11:11:28 +0000

    Old changes

--- solutions/ex1_atlie15.py
+++ solutions/ex1_atlie15.py
@@ -1,5 +1,6 @@
 import agent
 
+
 # select the agent here
 def get_agent(game):
     """Modify this method to create the appropriate agent"""
@@ -12,18 +13,32 @@ def get_agent(game):
         for ty in range(h):
             for cx in range(w):
                 for cy in range(h):
-                    player.policy[(cx,cy,tx,ty)] = define_policy(cx,cy,tx,ty,w,h)
+                    player.policy[(cx, cy, tx, ty)] = define_policy(cx, cy, tx, ty, w, h)
 
     return player
 
-def define_policy(cx,cy,tx,ty,w,h):
+
+def define_policy(cx, cy, tx, ty, w, h):
     """Defines the policy for the situation where the cheese is at [cx,cy],
     the trap is at [tx,ty], and the grid has width w and height h (all
     relative to the mouse)."""
 
     # put your own code for the policy here
 
-    return 'forward'
+    # Check if cheese is closer to the right
+    # And if there is a trap up close
+    prefer_right = tx == w - 1 or (cx <= w / 2 and tx != 1)
+    trap_in_front = ty == 1 and tx == 0
+
+    if cy != 0:
+        # Cheese is not horizontally aligned with mouse
+        if not trap_in_front:
+            return 'forward'
+
+    if prefer_right:
+        return 'right'
+    else:
+        return 'left'
 
 
 # train the agent before evaluation, but not in this exercise
--- solutions/ex2_atlie15.py
+++ solutions/ex2_atlie15.py
@@ -14,7 +14,7 @@ def train(player):
     """This method should be implemented by the student
     An example implementation is provided."""
     print 'Training...'
-    player.set_exploration_rate(0.25)  # set the exploration
+    player.set_exploration_rate(1)  # set the exploration
     for i in xrange(1000):  # train it for 1000 steps
         player.perform()     # do an action at every step
     player.set_exploration_rate(0.0) # turn exploration off for evaluation
